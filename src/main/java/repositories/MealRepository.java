03ece184e3dfde93d4e4aab1667f1a188c543c6e
Александр <Wolgrade@yandex.ru>
2025-03-15 18:36:53 +0400

    Добавление интерфейсов 3

--- src/main/java/repositories/MealRepository.java
+++ src/main/java/repositories/MealRepository.java
@@ -1,4 +1,23 @@
 package repositories;
 
-public interface MealRepository {
+import model.Meal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface MealRepository extends JpaRepository<Meal, Integer> {
+
+    List<Meal> findByUserId(int userId);
+    List<Meal> findByDateBetween(Date startDate, Date endDate);
+    List<Meal> findByDate(Date date);
+
+    // name LIKE 'value%'
+    List<Meal> findByNameStartingWith(String prefix);
+    // name LIKE '%value'
+    List<Meal> findByNameEndingWith(String suffix);
+
+    Meal findById(int id);
 }
