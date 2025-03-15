abc511fb5b7bc574a1c37d2223493690b8885a10
Александр <Wolgrade@yandex.ru>
2025-03-15 17:01:05 +0400

    Добавление методов в сервисы

--- src/main/java/services/UserService.java
+++ src/main/java/services/UserService.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import jakarta.persistence.*;
+import java.util.List;
+
 @Service
 public class UserService {
 
@@ -14,6 +17,12 @@ public class UserService {
     @Autowired
     private BCryptPasswordEncoder passwordEncoder; // Для хеширования паролей
 
+    private EntityManagerFactory emf;
+
+    public UserService() {
+        this.emf = Persistence.createEntityManagerFactory("myPersistenceUnit");
+    }
+
     public user registerUser(String login, String password, int Id) {
         user User = new user();
         User.setLogin(login);
@@ -22,4 +31,45 @@ public class UserService {
 
         return userRepository.save(User); // Сохранение пользователя в базе данных
     }
+
+    // метод для сохранения пользователя в базу данных
+    public void saveUser(user User) {
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        entityManager.persist(User);
+        transaction.commit();
+        entityManager.close();
+    }
+
+    public void updateUser(user User) {
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        entityManager.merge(User);
+        transaction.commit();
+        entityManager.close();
+    }
+
+    public void deleteUser(user User) {
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        // Check if the entity is managed by the EntityManager, and if not, merge it before removal
+        entityManager.remove(entityManager.contains(User) ? User : entityManager.merge(User));
+        transaction.commit();
+        entityManager.close();
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
+
